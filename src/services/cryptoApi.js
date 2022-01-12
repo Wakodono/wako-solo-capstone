25646059c9b1100e1ed879e77662399dc1a1bbef
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-01-12 18:40:33 +0000

    getCryptosQuery hook
    Created  a custom hook capable of handling fetches on as many specified endpoints as needed

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '87312c18b3msh91c5b85bff76aa1p1d1919jsn37d92e886837'
+
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: ( ) => createRequest('/coins')
+        })
+    })
+})
+
+/* var options = {
+    method: 'GET',
+    url: 'https://coinranking1.p.rapidapi.com/coins',
+    headers: {
+      'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+      'x-rapidapi-key': '87312c18b3msh91c5b85bff76aa1p1d1919jsn37d92e886837'
+    }
+  }; */
+
+  export const {
+      useGetCryptosQuery,
+  } = cryptoApi
