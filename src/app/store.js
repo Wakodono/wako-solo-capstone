f5faba2ea2a0480bd8d9115001af6c8e51f2fd71
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-01-21 18:32:42 +0000

    Update store.js
    Adds middleware that enables caching and other useful features of RTK Query
    
    This fixed the RTK Query warnings

--- src/app/store.js
+++ src/app/store.js
@@ -9,4 +9,8 @@ export default configureStore({
         [cryptoApi.reducerPath]: cryptoApi.reducer,
         [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
     },
+    // Adding the api middleware enables caching, invalidation, polling,
+    // and other useful features of `rtk-query`.
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(cryptoApi.middleware, cryptoNewsApi.middleware),
 })
