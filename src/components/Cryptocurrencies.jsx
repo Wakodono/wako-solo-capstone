3108d0f9e0a30aeb0570524d6497b19e39d2b1cc
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-02-14 14:32:31 +0000

    updates package.json

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -38,7 +38,7 @@ const Cryptocurrencies = ({ simplified }) => {
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
                         <Link to={`/crypto/${currency.uuid}`}>
                             <Card title={`${currency.rank}. ${currency.name}`} extra={<img className='crypto-image' src={currency.iconUrl} />} hoverable>
-                                <p>Price: {millify(currency.price)}</p>
+                                <p>Price: ${millify(currency.price)}</p>
                                 <p>Cap: {millify(currency.marketCap)}</p>
                                 <p>Daily Change: {millify(currency.change)}%</p>
                             </Card>
